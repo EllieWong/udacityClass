18b841c0ad918606fd63185b4fab3f28b2d4234a
vagrant <vagrant@vagrant.vm>
2019-05-15 02:11:17 +0000

    modify the Requires Changes first time

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
 # udacityClass
-
-1. Project1
-
-1.1 run the command first: create view log_view as select path,count(*) as num from log group by path having path!='/' order by num desc;
-
-1.2 run the command: python project1.py
+### Project Name: News Website Logs Analysis Statistics
+This project sets up a **PostgreSQL** database for a **news** website.
+The provided Python script **project1.py** uses the **psycopg2** library to query the database and produce a report that answers the following questions:
+1. What are the most popular three articles of all time?
+Answer:
+"Candidate is jerk, alleges rival" -- 338647 views
+"Bears love berries, alleges bear" -- 253801 views
+"Bad things gone, say good people" -- 170098 views
+2. Who are the most popular article authors of all time?
+Answer:
+Ursula La Multa -- 507594 views
+Rudolf von Treppenwitz -- 423457 views
+Anonymous Contributor -- 170098 views
+Markoff Chaney -- 84557 views
+3. On which days did more than 1% of requests lead to errors?
+Answer:
+Jul 17,2016 -- 2.26% errors
+#### Requirements
+- Python
+- PostgreSQL
+- psycopg2 library
+- Please download news database: https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip
+- Import news database: e.g. $ psql -d news -f news_sql_database_file.sql
+### Usage
+1. run the command first: create view log_view as select path,count(*) as num from log group by path having path!='/' order by num desc;
+2. run the command: python project1.py
--- project1.py
+++ project1.py
@@ -1,70 +1,94 @@
-#Projct1 homework
+#!/usr/bin/env python
+# Projct1 homework
 
 import psycopg2
 
-DBNAME='news'
+DBNAME = 'news'
+
 
 def getTop3Articles():
-  db = psycopg2.connect(database=DBNAME)
-  c = db.cursor()
-  c.execute("select title, num from articles join log_view on log_view.path=concat('/article/',articles.slug) order by num desc limit 3;")
-  results = c.fetchall()
-  db.close()
-  for result in results:
-    print ("\"%s\" -- %s views"%result)
-  
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute("""
+SELECT title,
+       num
+FROM articles
+JOIN log_view ON log_view.path=concat('/article/', articles.slug)
+ORDER BY num DESC
+LIMIT 3;
+""")
+    results = c.fetchall()
+    db.close()
+    for result in results:
+        # print ("\"%s\" -- %s views"%result)
+        print '"{article}" -- {count} views'.format(article=result[0], count=result[1])
+
 
 def getTopAuthors():
-  db = psycopg2.connect(database=DBNAME)
-  c = db.cursor()
-  c.execute("""select authors.name, article_log.pv
-from (
-    select article1.author as author, sum(log1.pv) as pv
-    from (
-        select author, slug, concat('/article/', slug) as path 
-        from articles 
-        group by author, slug, concat('/article/', slug)
-    ) article1
-    join (
-        select path, count(1) as pv 
-        from log group by path
-    ) log1
-    on article1.path = log1.path
-    group by article1.author
-) article_log
-join authors
-on article_log.author = authors.id
-order by article_log.pv desc;""")
-  results = c.fetchall()
-  db.close()
-  for result in results:
-    print("%s -- %s views"%result)
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute("""
+SELECT authors.name,
+       article_log.pv
+FROM
+  (SELECT article1.author AS author,
+          sum(log1.pv) AS pv
+   FROM
+     (SELECT author,
+             slug,
+             concat('/article/', slug) AS PATH
+      FROM articles
+      GROUP BY author,
+               slug,
+               concat('/article/', slug)) article1
+   JOIN
+     (SELECT PATH,
+             count(1) AS pv
+      FROM log
+      GROUP BY PATH) log1 ON article1.path = log1.path
+   GROUP BY article1.author) article_log
+JOIN authors ON article_log.author = authors.id
+ORDER BY article_log.pv DESC;
+""")
+    results = c.fetchall()
+    db.close()
+    for result in results:
+        print ("%s -- %s views" % result)
+
 
 def getFailurate():
-  db = psycopg2.connect(database=DBNAME)
-  c = db.cursor()
-  c.execute("""select * from (
-select total.date, round((cast(error.pv as numeric) / total.pv * 100), 2) as failure
-from (
-    select to_char(time,'Mon DD,YYYY') as date,count(*) as pv from log where status!='200 OK' group by date
-) as error 
-join (
-    select to_char(time,'Mon DD,YYYY') as date,count(*) as pv from log group by date 
-) as total
-on error.date=total.date
-) as final where failure>1;""")
-  results = c.fetchall()
-  db.close()
-  for result in results:
-    print("%s -- %0.2f%% errors"%result)
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute("""
+SELECT *
+FROM
+  (SELECT total.date,
+          round((cast(error.pv AS numeric) / total.pv * 100), 2) AS failure
+   FROM
+     (SELECT to_char(TIME, 'Mon DD,YYYY') AS date,
+             count(*) AS pv
+      FROM log
+      WHERE status!='200 OK'
+      GROUP BY date) AS error
+   JOIN
+     (SELECT to_char(TIME, 'Mon DD,YYYY') AS date,
+             count(*) AS pv
+      FROM log
+      GROUP BY date) AS total ON error.date=total.date) AS FINAL
+WHERE failure>1;
+""")
+    results = c.fetchall()
+    db.close()
+    for result in results:
+        print ("%s -- %0.2f%% errors" % result)
 
 
 if __name__ == '__main__':
-  print 'question1: What are the most popular three articles of all time? '
-  getTop3Articles()
-  print
-  print 'question2: Who are the most popular article authors of all time?'
-  getTopAuthors()
-  print 
-  print 'question3: On which days did more than 1% of requests lead to errors? '
-  getFailurate()
+    print 'question1: What are the most popular three articles of all time? '
+    getTop3Articles()
+    print
+    print 'question2: Who are the most popular article authors of all time?'
+    getTopAuthors()
+    print
+    print 'question3: On which days did more than 1% of requests lead to errors? '
+    getFailurate()
